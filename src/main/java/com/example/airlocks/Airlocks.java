d22233ba2cac0416ff415fd745f6b1a4ecda6071
Cerberus1470 <tejas.dhilip04@gmail.com>
2024-03-28 23:42:23 -0700

    Version 1.0.3 Added proper hitboxes to the Airlock Door.
    Full Changelog:
    Airlocks.java:
    - New method: rotateShape(). This is a garbage method, copy-pasted from online. It does not work for my purposes, and is marked deprecated.
    - New method: join(). This is a custom method by me to join multiple VoxelShapes together. As many as are needed! Hundreds... Thousands... There is no limit.
    - The Screen for BlockTesting is once again added, fixing the screen functionality.
    BlockAirlockDoor.java:
    - New AABBs: Closed, BottomLeftOpen, BottomRightOpen, TopLeftOpen, TopRightOpen. These are all arrays of AABBs that house a hitbox for each cardinal direction according to their state. They were made manually. Tedious work.
    - getShape() now calls the proper custom AABBs.
    All Classes:
    - Fixed @NotNull annotations, throwing away a lot of errors.
    - Fixed all the problems :)

--- src/main/java/com/example/airlocks/Airlocks.java
+++ src/main/java/com/example/airlocks/Airlocks.java
@@ -10,10 +10,13 @@ import com.example.airlocks.sound.ModSounds;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -62,6 +65,43 @@ public class Airlocks
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+
+            MenuScreens.register(ModMenuTypes.BLOCK_TESTING_MENU.get(), BlockTestingScreen::new);
+        }
+    }
+
+    @Deprecated
+    public static VoxelShape rotateShape(Direction from, Direction to, VoxelShape shape) {
+        VoxelShape[] buffer = new VoxelShape[]{shape, Shapes.empty()};
+
+        int times = (to.get2DDataValue() - from.get2DDataValue() + 4) % 4;
+        for (int i = 0; i < times; i++) {
+
+//            buffer[0].forAllBoxes((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] = Shapes.or(buffer[1], Shapes.create(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX)));
+//            buffer[0] = buffer[1];
+//            buffer[1] = Shapes.empty();
+        }
+
+        return buffer[0];
+    }
+
+    public static VoxelShape join(BooleanOp op, VoxelShape... shapes) {
+        VoxelShape shape = shapes[0];
+        for (int i=1 ; i<shapes.length; i++) {
+            shape = Shapes.join(shape, shapes[i], op);
+        }
+        return shape;
     }
 
 }
--- src/main/java/com/example/airlocks/block/custom/BlockAirlockDoor.java
+++ src/main/java/com/example/airlocks/block/custom/BlockAirlockDoor.java
@@ -1,5 +1,6 @@
 package com.example.airlocks.block.custom;
 
+import com.example.airlocks.Airlocks;
 import com.example.airlocks.sound.ModSounds;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -14,15 +15,131 @@ import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
+import net.minecraft.world.level.block.state.properties.DoorHingeSide;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 
 public class BlockAirlockDoor extends DoorBlock {
+        private static final VoxelShape[] CLOSED_AABB = {
+                Block.box(0,0,0,16,16,4), //SOUTH
+                Block.box(12,0,0,16,16,16), //WEST
+                Block.box(0,0,12,16,16,16), //NORTH
+                Block.box(0,0,0,4,16,16) //EAST
+        };
+        private static final VoxelShape[] BOTTOM_LEFT_OPEN_AABB = {
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(10.75,0.25,2,14.75,16,17.5),
+                        Block.box(0,0,0,16,0.25,4),
+                        Block.box(0,0,0,0.25,16,4),
+                        Block.box(15.75,0,0,16,16,4)
+                ), // SOUTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(-1.5,0.25,10.75,14,16,14.75),
+                        Block.box(12,0,0,16,0.25,16),
+                        Block.box(12,0,0,16,16,0.25),
+                        Block.box(12,0,15.75,16,16,16)
+                ), // WEST
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(1.25,0.25,-1.5,5.25,16,14),
+                        Block.box(0,0,12,16,0.25,16),
+                        Block.box(0,0,12,0.25,16,16),
+                        Block.box(15.75,0,12,16,16,16)
+                ), // NORTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(2,0.25,1.25,17.5,16,5.25),
+                        Block.box(0,0,0,4,0.25,16),
+                        Block.box(0,0,0,4,16,0.25),
+                        Block.box(0,0,15.75,4,16,16)
+                ) // EAST
+        };
+        private static final VoxelShape[] BOTTOM_RIGHT_OPEN_AABB = {
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(1.25,0.25,2,5.25,16,17.5),
+                        Block.box(0,0,0,16,0.25,4),
+                        Block.box(0,0,0,0.25,16,4),
+                        Block.box(15.75,0,0,16,16,4)
+                ), // SOUTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(-1.5,0.25,1.25,14,16,5.25),
+                        Block.box(12,0,0,16,0.25,16),
+                        Block.box(12,0,0,16,16,0.25),
+                        Block.box(12,0,15.75,16,16,16)
+                ), // WEST
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(10.75,0.25,-1.5,14.75,16,14),
+                        Block.box(0,0,12,16,0.25,16),
+                        Block.box(0,0,12,0.25,16,16),
+                        Block.box(15.75,0,12,16,16,16)
+                ), //NORTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(2,0.25,10.75,17.5,16,14.75),
+                        Block.box(0,0,0,4,0.25,16),
+                        Block.box(0,0,0,4,16,0.25),
+                        Block.box(0,0,15.75,4,16,16)
+                ) // EAST
+        };
+        private static final VoxelShape[] TOP_LEFT_OPEN_AABB = {
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(10.75,0,2,14.75,15.75,17.5),
+                        Block.box(0,15.75,0,16,16,4),
+                        Block.box(0,0,0,0.25,16,4),
+                        Block.box(15.75,0,0,16,16,4)
+                ), // SOUTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(-1.5,0,10.75,14,15.75,14.75),
+                        Block.box(12,15.75,0,16,16,16),
+                        Block.box(12,0,0,16,16,0.25),
+                        Block.box(12,0,15.75,16,16,16)
+                ), // WEST
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(1.25,0,-1.5,5.25,15.75,14),
+                        Block.box(0,15.75,12,16,16,16),
+                        Block.box(0,0,12,0.25,16,16),
+                        Block.box(15.75,0,12,16,16,16)
+                ), // NORTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(2,0,1.25,17.5,15.75,5.25),
+                        Block.box(0,15.75,0,4,16,16),
+                        Block.box(0,0,0,4,16,0.25),
+                        Block.box(0,0,15.75,4,16,16)
+                ), // NORTH
+        };
+        private static final VoxelShape[] TOP_RIGHT_OPEN_AABB = {
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(1.25,0,2,5.25,15.75,17.5),
+                        Block.box(0,15.75,0,16,16,4),
+                        Block.box(0,0,0,0.25,16,4),
+                        Block.box(15.75,0,0,16,16,4)
+                ), // SOUTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(-1.5,0,1.25,14,15.75,5.25),
+                        Block.box(12,15.75,0,16,16,16),
+                        Block.box(12,0,0,16,16,0.25),
+                        Block.box(12,0,15.75,16,16,16)
+                ), // WEST
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(10.75,0,-1.5,14.75,15.75,14),
+                        Block.box(0,15.75,12,16,16,16),
+                        Block.box(0,0,12,0.25,16,16),
+                        Block.box(15.75,0,12,16,16,16)
+                ), // NORTH
+                Airlocks.join(BooleanOp.OR,
+                        Block.box(2,0,10.75,17.5,15.75,14.75),
+                        Block.box(0,15.75,0,4,16,16),
+                        Block.box(0,0,0,4,16,0.25),
+                        Block.box(0,0,15.75,4,16,16)
+                ), // NORTH
+        };
+
+
         public BlockAirlockDoor(Properties properties) {
                 super(properties
                         .sound(SoundType.METAL)
@@ -33,7 +150,7 @@ public class BlockAirlockDoor extends DoorBlock {
 
         @Override
         @NotNull
-        public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        public InteractionResult use(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult result) {
                 state = state.cycle(OPEN);
                 level.setBlock(pos, state, 10);
                 level.playSound(player, pos, state.getValue(OPEN) ? ModSounds.BLOCK_DOOR_OPEN.get() : ModSounds.BLOCK_DOOR_CLOSE.get(), SoundSource.BLOCKS);
@@ -61,6 +178,23 @@ public class BlockAirlockDoor extends DoorBlock {
                 return InteractionResult.sidedSuccess(level.isClientSide);
         }
 
+
+        @NotNull
+        @Override
+        public VoxelShape getShape(BlockState state, @NotNull BlockGetter getter, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+                int index = state.getValue(FACING).get2DDataValue();
+                if (state.getValue(OPEN)) {
+                        if (state.getValue(HALF) == DoubleBlockHalf.LOWER) {
+                                return state.getValue(HINGE) == DoorHingeSide.LEFT ? BOTTOM_LEFT_OPEN_AABB[index] : BOTTOM_RIGHT_OPEN_AABB[index];
+                        } else {
+                                return state.getValue(HINGE) == DoorHingeSide.LEFT ? TOP_LEFT_OPEN_AABB[index] : TOP_RIGHT_OPEN_AABB[index];
+                        }
+//                        return OPEN_AABB[(state.getValue(HINGE) == DoorHingeSide.LEFT ? 0 : 4) + state.getValue(FACING).get2DDataValue()];
+                } else {
+                        return CLOSED_AABB[index];
+                }
+        }
+
         public BlockPos getConnectedCanvas(Level level, BlockState state, BlockPos door_pos) {
                 BlockPos hingeBlock;
                 switch (state.getValue(HINGE)) {
@@ -83,6 +217,6 @@ public class BlockAirlockDoor extends DoorBlock {
                 return false;
         }
 
-        public void neighborChanged(BlockState p_52776_, Level p_52777_, BlockPos p_52778_, Block p_52779_, BlockPos p_52780_, boolean p_52781_) {
+        public void neighborChanged(@NotNull BlockState p_52776_, @NotNull Level p_52777_, @NotNull BlockPos p_52778_, @NotNull Block p_52779_, @NotNull BlockPos p_52780_, boolean p_52781_) {
         }
 }
